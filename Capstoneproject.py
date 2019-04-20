30406eeab1acc7dfe340a2b8f2b68cc5eed46309
JiangEdwin <49808833+JiangEdwin@users.noreply.github.com>
2019-04-20 22:52:14 +0800

    Add files via upload

---
+++ Capstoneproject.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# This Jupyter Notebook is mainly used for the capstone project from Coursera. 
+
+# In[3]:
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
